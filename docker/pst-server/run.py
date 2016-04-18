26677ac3e0fe0c9a72726c69f8d7119c89882495
Zexi Li <zexi.li@aliyun.com>
2016-04-18 19:42:25 +0800

    docker/pst-server: add server.py to run in docker, use: docker run -it -d -e INFLUXDB_HOST=xxx -e INFLUXDB_USER=xxx -e INFLUXDB_PASS=xxx to run server.
    Signed-off-by: Zexi Li <zexi.li@aliyun.com>

---
+++ docker/pst-server/run.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+
+SRC = '/perf-scripts'
+LIB_PATH = SRC + '/lib'
+sys.path.insert(0, LIB_PATH)
+import common
+
+conf_file = SRC + '/etc/pst_server.yaml'
+conf_dict = common.load_conf(conf_file)
+conf_dict['pst_server']['port'] = os.getenv('PST_SERVER_PORT', 8080)
+conf_dict['influxdb']['ip'] = os.getenv('INFLUXDB_HOST', '127.0.0.1')
+conf_dict['influxdb']['port'] = os.getenv('INFLUXDB_PORT', 8086)
+conf_dict['influxdb']['user'] = os.getenv('INFLUXDB_USER', 'root')
+conf_dict['influxdb']['pass'] = os.getenv('INFLUXDB_PASS', 'root')
+conf_dict['influxdb']['dbname'] = os.getenv('INFLUXDB_DBNAME', 'pst_results')
+
+common.save_yaml(conf_file, conf_dict)
+
+subprocess.call('service mongodb start', shell=True)
+subprocess.call(SRC + '/server/server.py', shell=True)
