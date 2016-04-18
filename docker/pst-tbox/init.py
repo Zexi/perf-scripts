d6965748b2fcd57a5960bae4c33110234551ebb0
Zexi Li <zexi.li@aliyun.com>
2016-04-18 21:04:01 +0800

    docker/pst-tbox: add pst tbox to docker, this is a clear environment to run test.
    Signed-off-by: Zexi Li <zexi.li@aliyun.com>

---
+++ docker/pst-tbox/init.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+
+SRC = '/perf-scripts'
+LIB_PATH = SRC + '/lib'
+sys.path.insert(0, LIB_PATH)
+import common
+
+conf_file = SRC + '/etc/autorun_conf.yaml'
+conf_dict = common.load_conf(conf_file)
+conf_dict['runtime'] = os.getenv('RUNTIME', 300)
+conf_dict['server']['hostname'] = os.getenv('SERVER_IP', '127.0.0.1')
+conf_dict['server']['port'] = os.getenv('SERVER_PORT', 8080)
+conf_dict['server']['res'] = os.getenv('SERVER_RES', 'results')
+jobs_env = os.getenv('TBOX_JOBS', '').split(';')
+jobs = conf_dict['jobs']
+
+jobs = set(jobs + jobs_env)
+
+print "--------------------------------"
+print "Jobs been run: %s" % jobs
+print "--------------------------------"
+common.save_yaml(conf_file, conf_dict)
